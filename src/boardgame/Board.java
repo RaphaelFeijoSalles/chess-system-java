9a39ea1cbb7b24ebd4234d5f7763043bee6a8aa3
Raphael Salles <feijoraphael1@gmail.com>
2025-11-28 12:15:29 -0300

    Placing pieces on the bord

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -4,12 +4,12 @@ public class Board {
 
     private int rows;
     private int columns;
-    private Piece[][] piece;
+    private Piece[][] pieces;
 
     public Board(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
-        piece = new Piece[rows][columns];
+        pieces = new Piece[rows][columns];
     }
 
     public int getRows() {
@@ -29,12 +29,16 @@ public class Board {
     }
 
     public Piece getPiece(int row, int column){
-        return piece[row][column];
+        return pieces[row][column];
     }
 
     public Piece getPiece(Position position){
-        return piece[position.getRow()][position.getColumn()];
+        return pieces[position.getRow()][position.getColumn()];
     }
 
+    public void placePiece(Piece piece, Position position){
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
 
 }
