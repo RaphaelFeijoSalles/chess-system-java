ed5ea2e82bda1f69bdcc9fc32f8f77b3e230be79
Raphael Salles <feijoraphael1@gmail.com>
2025-11-28 10:00:41 -0300

    Position class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import boardgame.Position;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
 
     }
 }
